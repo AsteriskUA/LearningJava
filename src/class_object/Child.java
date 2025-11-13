c61981da38cebdec554b0919b3f6da0ae04d7bb7
Zero <proctopanda857@gmail.com>
2025-11-13 17:08:52 +0200

    interface new

---
+++ src/class_object/Child.java
@@ -0,0 +1,19 @@
+package class_object;
+
+public class Child {
+    private String name;
+    private int year;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Child(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+}
---
+++ src/class_object/ChildrenGarden.java
@@ -0,0 +1,13 @@
+package class_object;
+
+public class ChildrenGarden {
+    private Child[] children;
+
+    public Child[] getChildren() {
+        return children;
+    }
+
+    public ChildrenGarden(Child[] children) {
+        this.children = children;
+    }
+}
